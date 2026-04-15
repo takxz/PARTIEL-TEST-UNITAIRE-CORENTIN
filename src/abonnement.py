f34b406d50d2b2e31f44a7a688cc0e40c9e11c21
takxz <corentin.leys@gmail.com>
2026-04-15 11:33:21 +0200

    fix: ajustement de l'enum abonnement

--- src/abonnement.py
+++ src/abonnement.py
@@ -3,3 +3,6 @@ from enum import Enum
 class Abonnement(Enum):
     FORFAIT = "Forfait"
     TICKET = "Ticket"
+
+    def __str__(self):
+        return self.value
--- tests/test_adherent.py
+++ tests/test_adherent.py
@@ -4,4 +4,4 @@ from src.abonnement import Abonnement
 def test_representation_adherent():
 
     adherent = Adherent(id=1, type_abonnement=Abonnement.FORFAIT, solde=100)
-    assert repr(adherent) == "Adherent(id=1, type_abonnement=Abonnement.FORFAIT, solde=100)"
+    assert repr(adherent) == "Adherent(id=1, type_abonnement=Forfait, solde=100)"
